9de89ccebe15863f58de4e65fa763d11a3d67682
NickFrias <nicolas.friasreid@code.berlin>
2026-09-09 12:00:53 +0200

    Create Landing Page
    Point out to the new route of the landing page for the Not The Project Home page.

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use resources\views\landing;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
-Route::get('/dashboard', function () {
-    return view('userzone.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
